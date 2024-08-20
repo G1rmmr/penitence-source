6276476d04925938600b8ca17e75d72d307490d7
G1rmmr <52289794+G1rmmr@users.noreply.github.com>
2024-08-20 18:29:20 +0900

    Add basic ECS arch

---
+++ src/ECS.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdio>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <bitset>
+#include <array>
+
+namespace GAlpha
+{
+	class Component;
+	class Entity;
+
+	using ComponID = std::size_t;
+}

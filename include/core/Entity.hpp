8c77a7f5708436d5e0549145d592e8541b598668
G1rmmr <52289794+G1rmmr@users.noreply.github.com>
2024-10-31 23:50:32 +0900

    delete method in Entity

--- include/core/Entity.hpp
+++ include/core/Entity.hpp
@@ -16,77 +16,11 @@
 #pragma once
 
 #include <cstdint>
-#include <cassert>
-#include <memory>
-#include <typeindex>
-#include <unordered_map>
-#include <stdexcept>
-
-#include "components/Component.hpp"
 
 namespace G2D
 {
-    class Entity
+    namespace Entity
     {
-    public:
-        using ID = uint16_t;
-
-        Entity() = default;
-
-        Entity(ID id) : id(id)
-        {
-
-        }
-
-        Entity& operator=(const Entity& other)
-        {
-            if(this != &other)
-            {
-                this->components = other.components;
-                this->id = other.id;
-            }
-            
-            return *this;
-        }
-
-        inline const ID GetID() const
-        {
-            return id;
-        }
-
-        template <typename T>
-        inline bool HasComponent() const
-        {
-            return components.find(typeid(T)) != std::end(components);
-        }
-
-        template <typename T, typename... Args>
-        inline void AddComponent(Args&&... args)
-        {
-            assert(!HasComponent<T>() && "Entity already has this component type.");
-            components[typeid(T)] = std::make_shared<T>(std::forward<Args>(args)...);
-        }
-
-        template <typename T>
-        inline void RemoveComponent()
-        {
-            auto iter = components.find(typeid(T));
-            if(iter != std::end(components))
-                components.erase(iter);
-        }
-
-        template <typename T>
-        inline T& GetComponent() const
-        {
-            auto iter = components.find(typeid(T));
-            if(iter != std::end(components))
-                return *static_cast<T*>(iter->second.get());
-
-            throw std::runtime_error("Component not found");
-        }
-        
-    private:
-        std::unordered_map<std::type_index, std::shared_ptr<Component>> components;
-        ID id;
-    };
+        using ID = uint8_t;
+    }
 }
